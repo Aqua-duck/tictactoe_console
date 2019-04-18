631782d03d00f10ff5c7703cc84a0ed8336ad430
Nikita Ahsan <somethingrandom3bf@gmail.com>
2019-04-17 20:03:17 -0400

    new file:   tictactoe_console_better/App.config 	new file:   tictactoe_console_better/Program.cs 	new file:   tictactoe_console_better/Properties/AssemblyInfo.cs 	new file:   tictactoe_console_better/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   tictactoe_console_better/obj/Debug/tictactoe_console_better.csproj.CoreCompileInputs.cache 	new file:   tictactoe_console_better/obj/Debug/tictactoe_console_better.csprojAssemblyReference.cache 	new file:   tictactoe_console_better/tictactoe_console_better.csproj

---
+++ tictactoe_console_better/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tictactoe_console_better
+{
+    public interface IPlayer
+    {
+        void playerTurn();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Grid
+    {
+
+    }
+}
